269eb85099bfcc2209905aa0c29cafdf9c3ab5a5
Miroslav Svítok <mirosvitok@gmail.com>
2019-07-30 23:20:55 +0200

    Alter index when migrating sales_funnel_stats
    remp/crm#784

--- src/migrations/20190730105329_add_device_type_to_sales_funnel_stats.php
+++ src/migrations/20190730105329_add_device_type_to_sales_funnel_stats.php
@@ -8,7 +8,13 @@ class AddDeviceTypeToSalesFunnelStats extends AbstractMigration
     public function change()
     {
         $this->table('sales_funnels_stats')
-            ->addColumn('device_type', 'text', ['null' => true])
+            ->addColumn('device_type', 'string', ['null' => true])
             ->update();
+
+
+        $this->table('sales_funnels_stats')
+            ->removeIndex(['sales_funnel_id', 'date', 'type'])
+            ->addIndex(['sales_funnel_id', 'date', 'type', 'device_type'], ['name' => 'sales_funnel_id', 'unique' => true])
+            ->save();
     }
 }
