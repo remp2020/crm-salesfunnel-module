e150bcbbfbc4076a601434cd42afdfd400f9b77b
Miroslav Svítok <mirosvitok@gmail.com>
2019-07-31 10:58:53 +0200

    Fixed sales funnel migration order when removing index
    remp/crm#784

--- src/migrations/20190730105329_add_device_type_to_sales_funnel_stats.php
+++ src/migrations/20190730105329_add_device_type_to_sales_funnel_stats.php
@@ -8,12 +8,15 @@ class AddDeviceTypeToSalesFunnelStats extends AbstractMigration
     public function change()
     {
         $this->table('sales_funnels_stats')
+            ->removeIndex(['sales_funnel_id', 'date', 'type'])
+            ->update();
+
+        $this->table('sales_funnels_stats')
             ->addColumn('device_type', 'string', ['null' => true])
             ->update();
 
         $this->table('sales_funnels_stats')
-            ->removeIndex(['sales_funnel_id', 'date', 'type'])
-            ->addIndex(['sales_funnel_id', 'date', 'type', 'device_type'], ['name' => 'sales_funnel_id', 'unique' => true])
+            ->addIndex(['sales_funnel_id', 'date', 'type', 'device_type'], ['unique' => true])
             ->update();
     }
 }
