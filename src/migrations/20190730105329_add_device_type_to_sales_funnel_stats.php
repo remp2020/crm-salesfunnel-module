ea98da89245fd7b0e8ec87d602f52c443fff4504
Miroslav Svítok <mirosvitok@gmail.com>
2019-07-31 11:30:55 +0200

    Another attempt to fix migration
    remp/crm#784

--- src/migrations/20190730105329_add_device_type_to_sales_funnel_stats.php
+++ src/migrations/20190730105329_add_device_type_to_sales_funnel_stats.php
@@ -9,14 +9,8 @@ class AddDeviceTypeToSalesFunnelStats extends AbstractMigration
     {
         $this->table('sales_funnels_stats')
             ->removeIndex(['sales_funnel_id', 'date', 'type'])
-            ->update();
-
-        $this->table('sales_funnels_stats')
             ->addColumn('device_type', 'string', ['null' => true])
-            ->update();
-
-        $this->table('sales_funnels_stats')
-            ->addIndex(['sales_funnel_id', 'date', 'type', 'device_type'], ['unique' => true])
+            ->addIndex(['sales_funnel_id', 'date', 'type', 'device_type'], ['name' => 'sales_funnels_stats_unique' , 'unique' => true])
             ->update();
     }
 }
