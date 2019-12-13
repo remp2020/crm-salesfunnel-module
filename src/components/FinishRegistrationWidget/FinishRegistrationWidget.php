006113e084c72ebe15820d4f52041de6bd394cf4
Peter Dulacka <dulacka@gmail.com>
2019-12-13 11:26:30 +0100

    Removing link to MailModule from widget in SalesFunnelModule
    Widget using the link was refactored to redirect user based
    on the payment referer - unless the referer points to the
    sales funnel where the payment was created.
    
    remp/crm#1003

--- src/components/FinishRegistrationWidget/FinishRegistrationWidget.php
+++ src/components/FinishRegistrationWidget/FinishRegistrationWidget.php
@@ -19,8 +19,18 @@ class FinishRegistrationWidget extends BaseWidget
         return 'finishregistrationwidget';
     }
 
-    public function render()
+    public function render($payment)
     {
+        if (!$payment->referer) {
+            return;
+        }
+
+        if ($payment->sales_funnel && strpos($payment->referer, $payment->sales_funnel->url_key) !== false) {
+            // referer is just link to the sales funnel, we don't want to redirect there
+            return;
+        }
+
+        $this->template->referer = $payment->referer;
         $this->template->setFile(__DIR__ . '/' . $this->templateName);
         $this->template->render();
     }
