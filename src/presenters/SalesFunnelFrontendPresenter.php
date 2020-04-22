c96739311fbb6b856cecba13354a5820f4362edf
Ahmad Abu-Aysheh <ahmadabuaysheh@gmail.com>
2020-04-22 18:22:06 +0300

    Fixing incorrect donation payment item vat type

--- src/presenters/SalesFunnelFrontendPresenter.php
+++ src/presenters/SalesFunnelFrontendPresenter.php
@@ -479,7 +479,7 @@ class SalesFunnelFrontendPresenter extends FrontendPresenter
             if ($donationPaymentVat === null) {
                 throw new \Exception("Config 'donation_vat_rate' is not set");
             }
-            $paymentItemContainer->addItem(new DonationPaymentItem($this->translator->translate('payments.admin.donation'), $additionalAmount, $donationPaymentVat));
+            $paymentItemContainer->addItem(new DonationPaymentItem($this->translator->translate('payments.admin.donation'), $additionalAmount, (int)$donationPaymentVat));
         }
 
         // let modules add own items to PaymentItemContainer before payment is created
