3ea5ddf3be83d09e8d6f598128572f02735dd4c7
Matus Kalafut <kalafut.matus@gmail.com>
2021-01-12 14:11:37 +0000

    Add coupon filter field to users admin filter
    - **BREAKING**: Add `filter()` function to interface `FilterUsersFormDataProviderInterface`. Function is used to filter users based on fields added by dataprovider.
    
    - **BREAKING**: Rename `payments.dataprovider.list_filter_form` dataprovider placeholder to `payments.dataprovider.payments_filter_form`.
    
    - Add `FilterUsersFormDataProvider` to add coupon filter field to `UsersAdminFilterForm`.
    
    - Merge `FilterUsersSelectionDataProvider` to `FilterUsersFormDataProvider`. `FilterUsersFormDataProvider` implements `FilterUsersFormDataProviderInterface` which now provides adding fields to user filter form and filtering users by added fields.
    
    remp/crm#1663

--- src/SalesFunnelModule.php
+++ src/SalesFunnelModule.php
@@ -171,7 +171,7 @@ class SalesFunnelModule extends CrmModule
     public function registerDataProviders(DataProviderManager $dataProviderManager)
     {
         $dataProviderManager->registerDataProvider(
-            'payments.dataprovider.list_filter_form',
+            'payments.dataprovider.payments_filter_form',
             $this->getInstance(PaymentsAdminFilterFormDataProvider::class)
         );
 
