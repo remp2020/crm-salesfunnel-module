594f5bc93e6259cec64d64c9e53ce5b111179836
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-10-11 12:03:43 +0200

    Transform @param's type `array` to `array{key: type}`
    Codesniffer removes import for hinted type if annotation is in format:
    
    ```
    /**
     * @param array $params {
     *   @type ActiveRow $user
     * }
     */
    ```
    
    It doesn't remove it if format is:
    
    ```
    /**
     * @param array{user: ActiveRow} $params
     */
    ```
    
    Fixed only arrays where Codesniffer removed imports.
    
    remp/crm#2057

--- src/dataproviders/ValidateUserFunnelAccessDataProviderInterface.php
+++ src/dataproviders/ValidateUserFunnelAccessDataProviderInterface.php
@@ -2,9 +2,16 @@
 
 namespace Crm\SalesFunnelModule\DataProvider;
 
+use Crm\ApplicationModule\DataProvider\DataProviderException;
 use Crm\ApplicationModule\DataProvider\DataProviderInterface;
+use Nette\Database\Table\ActiveRow;
 
 interface ValidateUserFunnelAccessDataProviderInterface extends DataProviderInterface
 {
-
+    /**
+     * @param array{sales_funnel: ActiveRow, user: ActiveRow} $params
+     * @return bool
+     * @throws DataProviderException
+     */
+    public function provide(array $params): bool;
 }
