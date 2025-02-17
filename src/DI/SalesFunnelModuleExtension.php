70c6896f3a0896c6af2612fa8aefab1710db643c
Peter Dulacka <dulacka@gmail.com>
2025-02-17 06:16:05 +0000

    Update Nette extension config definition to use config definition schema
    remp/crm#3417

--- src/DI/SalesFunnelModuleExtension.php
+++ src/DI/SalesFunnelModuleExtension.php
@@ -5,24 +5,22 @@ namespace Crm\SalesFunnelModule\DI;
 use Contributte\Translation\DI\TranslationProviderInterface;
 use Nette\Application\IPresenterFactory;
 use Nette\DI\CompilerExtension;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
 
 class SalesFunnelModuleExtension extends CompilerExtension implements TranslationProviderInterface
 {
     const PARAM_FUNNEL_ROUTES = 'funnel_routes';
 
-    private $defaults = [
-        self::PARAM_FUNNEL_ROUTES => true,
-    ];
+    public function getConfigSchema(): Schema
+    {
+        return Expect::structure([
+            self::PARAM_FUNNEL_ROUTES => Expect::bool(true)->dynamic(),
+        ]);
+    }
 
     public function loadConfiguration()
     {
-        $builder = $this->getContainerBuilder();
-        // set default values if user didn't define them
-        $this->config = $this->validateConfig($this->defaults);
-
-        // set extension parameters for use in config
-        $builder->parameters['funnel_routes'] = $this->config['funnel_routes'];
-
         // load services from config and register them to Nette\DI Container
         $this->compiler->loadDefinitionsFromConfig(
             $this->loadFromFile(__DIR__.'/../config/config.neon')['services']
@@ -32,13 +30,14 @@ class SalesFunnelModuleExtension extends CompilerExtension implements Translatio
     public function beforeCompile()
     {
         $builder = $this->getContainerBuilder();
+
         // load presenters from extension to Nette
         $builder->getDefinition($builder->getByType(IPresenterFactory::class))
             ->addSetup('setMapping', [['SalesFunnel' => 'Crm\SalesFunnelModule\Presenters\*Presenter']]);
 
         // configure API client
         $builder->getDefinitionByType(Config::class)
-            ->addSetup('setFunnelRoutes', [$this->config[self::PARAM_FUNNEL_ROUTES]]);
+            ->addSetup('setFunnelRoutes', [$this->config->{self::PARAM_FUNNEL_ROUTES}]);
     }
 
     /**
